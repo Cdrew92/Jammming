38e71fcd5682beaeba25d3b29b2afb250af6a336
Cdrew92 <39171327+Cdrew92@users.noreply.github.com>
2021-01-21 09:33:06 -0600

    added onclick to run search method

--- jammming/src/Components/Playlist/Playlist.js
+++ jammming/src/Components/Playlist/Playlist.js
@@ -19,7 +19,7 @@ render() {
         <div className="Playlist">
         <input onChange={this.handleNameChange} defaultValue='New Playlist'/>
         <TrackList isRemoval={true} onRemove={this.props.onRemove} tracks={this.props.playlistTracks}/>
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <button onClick={this.props.onSave} className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
         )
     }
