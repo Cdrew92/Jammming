52e86e0a0ef348ef5ff0b467e1bd18d8f27a394b
Cdrew92 <39171327+Cdrew92@users.noreply.github.com>
2021-01-22 08:15:29 -0600

    added line to delete results when add to playlist

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -42,6 +42,7 @@ class App extends React.Component {
     if (this.state.searchResults.length == removeDuplicates.length){
       return;
     }
+    this.setState({ searchResults: removeDuplicates })
   }
 
   addTrack(track) {
