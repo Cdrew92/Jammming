4f1fdf9ad60dcc25b70c14312f8ba7c1bc82fe14
Cdrew92 <39171327+Cdrew92@users.noreply.github.com>
2021-01-25 11:30:26 -0600

    hide api key

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -1,5 +1,5 @@
-const clientId = "631cff8bc3774a539eb31b67ea05f6c9";
-const redirectUri = "http://uselessmusicapp.surge.sh";
+const clientId = process.env.REACT_APP_API_KEY;
+const redirectUri = "http://spottyplaylist.surge.sh";
 
 let accessToken;
 
